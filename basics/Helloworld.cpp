ffc01c82031d43be81223dbfe5dc85d241eefd8a
ladesai123 <ladesaiteja24@gmail.com>
2024-07-13 00:58:47 +0530

    updated

--- basics/Helloworld.cpp
+++ basics/Helloworld.cpp
@@ -1,7 +1,30 @@
+//This is beginning of a Helloworld program
+
 #include<iostream>
+
+using namespace std;
+
+int global = 6;
+
+void func() {
+
+    int a = 5;
+}
 
 int main() {
 
-    std::cout<<"Hey welcome to cpp world"<<std::endl;
+    //Variable types..
+    int number = 10;
+    float price = 12.0;
+    char word = 'w';
+    string name = "saiteja";
+    
+
+    //Types of VAriables
+    //1. Local Variable
+    //2. Static Variable
+    //3. Instance Variable
+    
+    std::cout<<"Hey welcome to cpp world"<<endl;
     return 0;
 }
